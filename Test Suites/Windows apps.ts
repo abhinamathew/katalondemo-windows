6a08f98a3e019281818a27194055e162f1c32e31
abhin <abhin@LAPTOP-OK64MPKF>
2023-06-05 11:35:04 +0530

    Created test suite

---
+++ Test Suites/Windows apps.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Windows apps</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>86b1d5b7-12d8-42f8-85e3-28c6dd328e30</testSuiteGuid>
+   <testCaseLink>
+      <guid>dd49fc32-b24d-437f-ace4-0de34ae0ab42</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Calculator</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
